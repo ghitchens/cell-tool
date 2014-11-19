b65642d54824bd995d6026da031b6a2ddd00f687
Garth Hitchens <ghitchens@me.com>
2014-11-19 14:12:13 -0800

    fixed for new formats

--- lib/cmd/discover.ex
+++ lib/cmd/discover.ex
@@ -1,5 +1,6 @@
 defmodule Cmd.Discover do
-
+  require Logger
+  
   def run(spec, _opts \\ %{}) do
 		HTTPotion.start
     Finder.apply spec, "NAME\tIP ADDRESS\tSERIAL#\t\tTYPE\tVERSION -",
@@ -10,17 +11,34 @@ defmodule Cmd.Discover do
 		location = c.location
     {_, _, _, n} = c.ip
 		case Jrtp.get_services(location) do
-			{:ok, svcs} ->
-				srd = svcs.root
-        fw = svcs.firmware
-        case fw.firmware_status do
-          "normal" ->
-    				".#{n}\t#{Inet.ntoa(c.ip)}\t#{srd.serial_number}\t#{srd.model}\t#{fw.version}"
-          fw_status ->
-    				".#{n}\t#{Inet.ntoa(c.ip)}\t#{srd.serial_number}\t#{fw.model}\t#{fw.version} (#{fw_status})"
-        end
 			{:error, _x} ->
 				".#{n}\t#{Inet.ntoa(c.ip)}\t[unknown]\tmalfunctioning [server: #{c.server}]"
+			{:ok, svcs} ->
+        case svcs.root.description do     
+          description when is_bitstring(description) -> # v2
+            sf = svcs.firmware
+            sn = svcs.root.serial_number            
+            model = svcs.root.model
+            case sf[:info] do 
+              nil -> 
+                fv = "BROKEN"
+                fs = "BROKEN"
+              fi -> 
+                fv = fi.version
+                fs = fi.status
+            end
+          srd -> # v1
+            fv = srd.firmware_version
+            fs = srd.firmware_status
+            sn = srd.serial_number
+            model = srd.device_id
+        end
+        case fs do
+          "normal" ->
+            ".#{n}\t#{Inet.ntoa(c.ip)}\t#{sn}\t#{model}\t#{fv}"
+          fw_status ->
+            ".#{n}\t#{Inet.ntoa(c.ip)}\t#{sn}\t#{model}\t#{fv} (#{fs})"
+        end
 		end
   end
 
