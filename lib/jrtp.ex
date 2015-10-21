b64ee4d8fae6980f7060073ac84446021f6b6e2c
Milton Mazzarri <mmazzarris@suppliers.tenaris.com>
2015-10-21 00:17:18 -0500

    Improve data transformation for JRTP module

--- lib/jrtp.ex
+++ lib/jrtp.ex
@@ -9,18 +9,18 @@ defmodule Jrtp do
   @doc "gets cells services document and returns {:ok, services}]"
   def get_services(cell) do
     # Logger.info "#{inspect cell}"
-    case cell[:location] do
-      nil ->
-        { :error, :no_location }
-      location ->
-        full_url = Path.join(location, "services")
-        resp = HTTPotion.get full_url
-        case resp.status_code do
-          200 ->
-            JSX.decode resp.body, [{:labels, :atom}]
-          x ->
-            raise "services request for #{full_url} failed with error #{x}"
-        end
-    end
+    cell[:location]
+    |> cell_location()
   end
+
+  defp cell_location(nil), do: {:error, :no_location}
+  defp cell_location(location) do
+    location
+    |> Path.join("services")
+    |> HTTPotion.get()
+    |> verify_status()
+  end
+
+  defp verify_status({:ok, %HTTPotion.Response{status_code: 200, body: body}}), do: JSX.decode(body, [{:labels, :atom}])
+  defp verify_status({:ok, %HTTPotion.Response{status_code: x}}), do: raise "services request failed with error #{x}"
 end
