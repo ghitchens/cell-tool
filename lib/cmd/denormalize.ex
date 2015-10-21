db818cdb87b03cd9c7e23e78333188a7b11acdbb
Milton Mazzarri <mmazzarris@suppliers.tenaris.com>
2015-10-21 00:12:05 -0500

    Improve data transformation for denormalize cmd

--- lib/cmd/denormalize.ex
+++ lib/cmd/denormalize.ex
@@ -12,17 +12,17 @@ defmodule Cmd.Denormalize do
   end
 
   defp normalize(cell) do
-    location = cell.location
-    url = Path.join location, "/sys/firmware/current"
-    IO.write "#{cell.name} -> "
-    resp = HTTPotion.put(url, ~s({"status": "provisional"}), ["Content-Type": "application/json"])
-    case resp.status_code do
-      200 ->
-        IO.write "ok\n"
-      400 ->
-        IO.write "already provisional\n"
-      x ->
-        IO.write "DENORMALIZATION FAILED (ERROR #{x})\n"
-    end
+    cell.location
+    |> Path.join("/sys/firmware/current")
+    |> HTTPotion.put(~s({"status": "provisional"}), ["Content-Type": "application/json"])
+    |> verify_status()
+    |> response("#{cell.name} -> ")
+    |> IO.write()
   end
+
+  defp verify_status({:ok, %HTTPotion.Response{status_code: 200}}), do: "ok\n"
+  defp verify_status({:ok, %HTTPotion.Response{status_code: 400}}), do: "already provisional\n"
+  defp verify_status({:ok, %HTTPotion.Response{status_code: x}}), do: "DENORMALIZATION FAILED (ERROR #{x})\n"
+
+  defp response(message, prefix), do: "#{prefix} #{message}"
 end
