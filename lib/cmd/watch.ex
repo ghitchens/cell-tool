b931f9234c1b12e90374fc6e87f205da39edc4b7
Garth Hitchens <ghitchens@me.com>
2016-08-12 08:40:07 -0700

    updated watch functionality for nerves

--- lib/cmd/watch.ex
+++ lib/cmd/watch.ex
@@ -1,4 +1,4 @@
-defmodule Nerves.CLI.Cell.Cmd.Watch do
+defmodule Nerves.Cell.CLI.Cmd.Watch do
   @moduledoc """
   Watches the Logging UDP stream of all cells or the specified cell. The output
   is prepended by the last octet of the IP address then the logging message.
@@ -12,23 +12,23 @@ defmodule Nerves.CLI.Cell.Cmd.Watch do
 
   alias Nerves.Cell.CLI.Inet
 
-  @doc "Starts the watch on all cells"
-  def run do
+  @doc "Starts the watch on a single cell"
+  def run(%{cells: [cell]}=_context) do
+    IO.puts "Watching log from device on #{cell.host}"
+    {:ok, socket} = setup_socket
+    watch_mcast(socket, %{ip: cell.host})
+  end
+  def run(_context) do  # watch on more than one, or all
+    IO.puts "Watching log from all cells on LAN"
     {:ok, socket} = setup_socket
     watch_mcast(socket)
-  end
-
-  @doc "Starts the watch on the specified cells"
-  def run(ip_addr) do
-    {:ok, socket} = setup_socket
-    watch_mcast(socket, %{ip: ip_addr})
   end
 
   defp setup_socket do
     {:ok, socket} = :gen_udp.open @mcast_log_port, multicast_loop: false,
        multicast_if: {0, 0, 0, 0}, multicast_ttl: 4
     :ok = :inet.setopts socket, add_membership: {@mcast_log_group, {0, 0, 0, 0}}
-    IO.write "watching log from multicast group #{Inet.ntoa(@mcast_log_group)}:#{@mcast_log_port}\n\n"
+    IO.puts "Watching log from multicast group #{Inet.ntoa(@mcast_log_group)}:#{@mcast_log_port}\n"
     {:ok, socket}
   end
 
