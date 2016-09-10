b03b42a9fb058c3c0542d4dd40ee5dd243967ec1
Garth Hitchens <ghitchens@me.com>
2016-09-10 12:28:59 -0700

    improved watch filter and display

--- lib/cmd/watch.ex
+++ lib/cmd/watch.ex
@@ -9,6 +9,7 @@ defmodule Nerves.Cell.CLI.Cmd.Watch do
 
   @mcast_log_group {224, 0, 0, 224}
   @mcast_log_port 9999
+  @label_length   15
 
   alias Nerves.Cell.CLI.Inet
   alias Nerves.Cell.CLI.Finder
@@ -22,14 +23,18 @@ defmodule Nerves.Cell.CLI.Cmd.Watch do
   defp watch_devices(context) do
     {:ok, socket} = setup_socket
     count = Enum.count(context.cells)
-    IO.puts "Found #{count} cells"
     all = (context.args == [])
-    if ((not all) and count == 0) do
-      IO.puts "(omit filter to watch all LAN activity)"
-      :erlang.halt(1)
+    cond do
+      all ->
+        IO.puts "Watching all cells on LAN (#{count} discovered)"
+      count == 0 ->
+        IO.puts "No cells being watched (omit filter to watch all LAN activity)"
+        :erlang.halt(1)
+      true ->
+        IO.puts "Watching #{count} cell(s)"
     end
     context.cells
-    |> Enum.map(fn({id, cell}) -> {cell[:ip], id} end)
+    |> Enum.map(fn({id, cell}) -> {cell[:host], id} end)
     |> Enum.into(%{})
     |> watch_ips(socket, all)
   end
@@ -38,23 +43,30 @@ defmodule Nerves.Cell.CLI.Cmd.Watch do
   defp watch_ips(ips_map, socket, all) do
     receive do
       {:udp, _rcv_socket, source_ip, _port, msg} ->
-        case {ips_map[source_ip], all} do
-          {nil, true} ->
-            IO.write "#{inspect source_ip}\t#{msg}"
-          {nil, _} ->
-            nil
-          {id, _} ->
-            IO.write "#{id}\t#{msg}"
+        ascii_ip =
+          :inet.ntoa(source_ip)
+          |> to_string
+        case {ips_map[ascii_ip], all} do
+          {nil, true} ->  format_log_msg(ascii_ip, msg)
+          {nil, _} ->     nil
+          {id, _} ->      format_log_msg(id, msg)
         end
         watch_ips(ips_map, socket, all)
     end
+  end
+
+  defp format_log_msg(from, msg) do
+    label =
+      (from <> ":")
+      |> String.pad_trailing(@label_length)
+    IO.write label <> (to_string msg)
   end
 
   defp setup_socket do
     {:ok, socket} = :gen_udp.open @mcast_log_port, multicast_loop: false,
        multicast_if: {0, 0, 0, 0}, multicast_ttl: 4
     :ok = :inet.setopts socket, add_membership: {@mcast_log_group, {0, 0, 0, 0}}
-    IO.puts "Watching log from multicast group #{Inet.ntoa(@mcast_log_group)}:#{@mcast_log_port}\n"
+    IO.puts "Watching log from multicast group #{Inet.ntoa(@mcast_log_group)}:#{@mcast_log_port}"
     {:ok, socket}
   end
 
